5e2c35b5e563018e4b59f0fa0fbda4554d7df7ed
scatt89 <dasilvagarciam@gmail.com>
2016-03-24 13:47:36 +0100

    Skeleton application

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name webAppApp
+ * @description
+ * # webAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('webAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ]);
