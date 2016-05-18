8e0e7511fa1473d658da6b3e693915b790cdf9dc
scatt89 <dasilvagarciam@gmail.com>
2016-05-18 22:53:55 +0200

    elasticsearch client added and test cluster connection

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,8 +8,9 @@
  *
  * Main module of the application.
  */
-angular
+var app = angular
   .module('webAppApp', [
+    'elasticsearch',
     'ngAnimate',
     'ngCookies',
     'ngResource',
@@ -17,3 +18,24 @@ angular
     'ngSanitize',
     'ngTouch'
   ]);
+
+// Create the es service from the esFactory
+app.service('es', function (esFactory) {
+  return esFactory({ host: 'localhost:9200' });
+});
+
+// We define an Angular controller that returns the server health
+// Inputs: $scope and the 'es' service
+app.controller("ConnectionController", ['es', function(es){
+  this.info = "";
+  var self = this;
+
+  es.cluster.health(function (err, resp) {
+    if (err) {
+      self.info = err.message;
+    } else {
+      self.info = "Elasticsearch connected!";
+    }
+  });
+
+}]);
