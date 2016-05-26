37aee25c55b88ed63d73ffd5fc001d7a4ab095c2
scatt89 <dasilvagarciam@gmail.com>
2016-05-26 20:49:32 +0200

    Trying cut the field _source.log in sub fields but get error undefined when the object is readed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -46,36 +46,64 @@ app.controller("QueryController", ['es','$log', function(es,$log){
 
   // search for documents
   es.search({
-    index: 'logstash-2016.05.23',
+    index: 'logstash-2016.05.24',
     size: 1000,
     _source:["@timestamp", "container_name", "log"],
     body:{
       "query":{"match_all": {}}
     }
   }).then(function(response){
-    vm.hits = response.hits.hits;
+    var prehits = response.hits.hits;
+    //vm.hits = parseLogs(response.hits.hits);
+
+
+
+    vm.hits = function(){
+      var new_hits = [];
+      var spring_log_regex = /^((\d{1,4}\-\d{1,2}\-\d{1,2})\s+(\d{1,2}\:\d{1,2}\:\d{1,2}\.\d{1,4})\s+(ERROR|WARN|INFO|DEBUG|TRACE|FATAL)\s+(\d+)\s+(---)\s+(\[[\w\-]*\])\s+([\w\.\[\]\/\\]*)\s+\:\s+(.*))/;
+      var hit;
+      for(hit in prehits){
+        $log.debug(hit);
+        if(hit._source.log!==undefined){
+          if(spring_log_regex.exec(hit._source.log)){
+            var new_source = [];
+            new_source.time=hit._source.log.match(/^((\d{1,4}\-\d{1,2}\-\d{1,2})\s+(\d{1,2}\:\d{1,2}\:\d{1,2}\.\d{1,4}))/); //match timestamp
+            new_source.container_name=hit._source.container_name; //take the container name
+            new_source.log_level=hit._source.log.match(/(ERROR|WARN|INFO|DEBUG|TRACE|FATAL)/); //match the log level
+            new_source.process_id=hit._source.log.match(/\s+(\d+)\s+/); //match the process id
+            new_source.thread_name=hit._source.log.match(/\s+(\[[\w\-]*\])\s+/);
+            new_source.class=hit._source.log.match(/\s+([\w\.\[\]\/\\]*)\s+\:/);
+            new_source.message=hit._source.log.match(/\:\s+(.*)$/);
+            new_hits.push(new_source);
+          }
+        }
+      }
+      return new_hits;
+    }
   }, function(error){
     $log.debug(error);
   });
 
 }]);
 
-//Controller that returns query results
-// app.controller("QueryController", ['es','$log', function(es,$log){
-//
-//   var vm = this;
-//
-//   // search for documents
-//   es.get({
-//     index: 'logstash-2016.05.23',
-//     type: '_all',
-//     _source:["@timestamp", "container_name", "log"]
-//   }, function (error, response){
-//     if(error){
-//       $log.debug(error);
-//     }else{
-//       vm = response.hits.hits;
+// var parseLogs = function(hits){
+//   var new_hits = [];
+//   var spring_log_regex = /^((\d{1,4}\-\d{1,2}\-\d{1,2})\s+(\d{1,2}\:\d{1,2}\:\d{1,2}\.\d{1,4})\s+(ERROR|WARN|INFO|DEBUG|TRACE|FATAL)\s+(\d+)\s+(---)\s+(\[[\w\-]*\])\s+([\w\.\[\]\/\\]*)\s+\:\s+(.*))/;
+//   var hit;
+//   for(hit in hits){
+//     if(hit._source.log!==undefined){
+//       if(spring_log_regex.exec(hit._source.log)){
+//         var new_source = [];
+//         new_source.time=hit._source.log.match(/^((\d{1,4}\-\d{1,2}\-\d{1,2})\s+(\d{1,2}\:\d{1,2}\:\d{1,2}\.\d{1,4}))/); //match timestamp
+//         new_source.container_name=hit._source.container_name; //take the container name
+//         new_source.log_level=hit._source.log.match(/(ERROR|WARN|INFO|DEBUG|TRACE|FATAL)/); //match the log level
+//         new_source.process_id=hit._source.log.match(/\s+(\d+)\s+/); //match the process id
+//         new_source.thread_name=hit._source.log.match(/\s+(\[[\w\-]*\])\s+/);
+//         new_source.class=hit._source.log.match(/\s+([\w\.\[\]\/\\]*)\s+\:/);
+//         new_source.message=hit._source.log.match(/\:\s+(.*)$/);
+//         new_hits.push(new_source);
+//       }
 //     }
-//   });
-//
-// }]);
+//   }
+//   return new_hits;
+// };
