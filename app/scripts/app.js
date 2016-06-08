aab96edc3056662165d3740efbdba66252eb7b7f
scatt89 <dasilvagarciam@gmail.com>
2016-06-08 22:46:14 +0200

    modified some code with ng-bind directive instead {{}} characters

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -46,64 +46,43 @@ app.controller("QueryController", ['es','$log', function(es,$log){
 
   // search for documents
   es.search({
-    index: 'logstash-2016.05.24',
+    index: 'fluentd',
     size: 1000,
     _source:["@timestamp", "container_name", "log"],
     body:{
       "query":{"match_all": {}}
     }
   }).then(function(response){
-    var prehits = response.hits.hits;
-    //vm.hits = parseLogs(response.hits.hits);
+    //vm.hits=  response.hits.hits;
+    vm.hits = parseLogs(response.hits.hits);
 
-
-
-    vm.hits = function(){
-      var new_hits = [];
-      var spring_log_regex = /^((\d{1,4}\-\d{1,2}\-\d{1,2})\s+(\d{1,2}\:\d{1,2}\:\d{1,2}\.\d{1,4})\s+(ERROR|WARN|INFO|DEBUG|TRACE|FATAL)\s+(\d+)\s+(---)\s+(\[[\w\-]*\])\s+([\w\.\[\]\/\\]*)\s+\:\s+(.*))/;
-      var hit;
-      for(hit in prehits){
-        $log.debug(hit);
-        if(hit._source.log!==undefined){
-          if(spring_log_regex.exec(hit._source.log)){
-            var new_source = [];
-            new_source.time=hit._source.log.match(/^((\d{1,4}\-\d{1,2}\-\d{1,2})\s+(\d{1,2}\:\d{1,2}\:\d{1,2}\.\d{1,4}))/); //match timestamp
-            new_source.container_name=hit._source.container_name; //take the container name
-            new_source.log_level=hit._source.log.match(/(ERROR|WARN|INFO|DEBUG|TRACE|FATAL)/); //match the log level
-            new_source.process_id=hit._source.log.match(/\s+(\d+)\s+/); //match the process id
-            new_source.thread_name=hit._source.log.match(/\s+(\[[\w\-]*\])\s+/);
-            new_source.class=hit._source.log.match(/\s+([\w\.\[\]\/\\]*)\s+\:/);
-            new_source.message=hit._source.log.match(/\:\s+(.*)$/);
-            new_hits.push(new_source);
-          }
-        }
-      }
-      return new_hits;
-    }
   }, function(error){
     $log.debug(error);
   });
 
 }]);
 
-// var parseLogs = function(hits){
-//   var new_hits = [];
-//   var spring_log_regex = /^((\d{1,4}\-\d{1,2}\-\d{1,2})\s+(\d{1,2}\:\d{1,2}\:\d{1,2}\.\d{1,4})\s+(ERROR|WARN|INFO|DEBUG|TRACE|FATAL)\s+(\d+)\s+(---)\s+(\[[\w\-]*\])\s+([\w\.\[\]\/\\]*)\s+\:\s+(.*))/;
-//   var hit;
-//   for(hit in hits){
-//     if(hit._source.log!==undefined){
-//       if(spring_log_regex.exec(hit._source.log)){
-//         var new_source = [];
-//         new_source.time=hit._source.log.match(/^((\d{1,4}\-\d{1,2}\-\d{1,2})\s+(\d{1,2}\:\d{1,2}\:\d{1,2}\.\d{1,4}))/); //match timestamp
-//         new_source.container_name=hit._source.container_name; //take the container name
-//         new_source.log_level=hit._source.log.match(/(ERROR|WARN|INFO|DEBUG|TRACE|FATAL)/); //match the log level
-//         new_source.process_id=hit._source.log.match(/\s+(\d+)\s+/); //match the process id
-//         new_source.thread_name=hit._source.log.match(/\s+(\[[\w\-]*\])\s+/);
-//         new_source.class=hit._source.log.match(/\s+([\w\.\[\]\/\\]*)\s+\:/);
-//         new_source.message=hit._source.log.match(/\:\s+(.*)$/);
-//         new_hits.push(new_source);
-//       }
-//     }
-//   }
-//   return new_hits;
-// };
+var parseLogs = function(hits){
+
+    var new_hits = [];
+    var spring_log_regex = /^((\d{1,4}\-\d{1,2}\-\d{1,2})\s+(\d{1,2}\:\d{1,2}\:\d{1,2}\.\d{1,4})\s+(ERROR|WARN|INFO|DEBUG|TRACE|FATAL)\s+(\d+)\s+(---)\s+(\[[\w\-]*\])\s+([\w\.\[\]\/\\]*)\s+\:\s+(.*))/;
+    var hit;
+    for(hit in hits){
+      //$log.debug(hit);
+      if(hit._source.log){
+        if(spring_log_regex.exec(hit._source.log)){
+          var new_source = [];
+          new_source.time=hit._source.log.match(/^((\d{1,4}\-\d{1,2}\-\d{1,2})\s+(\d{1,2}\:\d{1,2}\:\d{1,2}\.\d{1,4}))/); //match timestamp
+          new_source.container_name=hit._source.container_name; //take the container name
+          new_source.log_level=hit._source.log.match(/(ERROR|WARN|INFO|DEBUG|TRACE|FATAL)/); //match the log level
+          new_source.process_id=hit._source.log.match(/\s+(\d+)\s+/); //match the process id
+          new_source.thread_name=hit._source.log.match(/\s+(\[[\w\-]*\])\s+/);
+          new_source.class=hit._source.log.match(/\s+([\w\.\[\]\/\\]*)\s+\:/);
+          new_source.message=hit._source.log.match(/\:\s+(.*)$/);
+          new_hits.push(new_source);
+        }
+      }
+    }
+    return new_hits;
+
+};
